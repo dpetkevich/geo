060e8ed1fd994fb9c2b858ce52b6744a988adaf9
Daniel Petkevich <daniel.petkevich@gmail.com>
2011-12-26 22:38:16 -0600

    live try 2

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
 $(  document).ready( function () {
     
-    var socket = io.connect( 'http://localhost' );
+    var socket = io.connect( 'http://http://simple-night-1895.herokuapp.com/' );
     socket.on( 'new_post_created', function ( data ) {
       createPost( data );
     } );
