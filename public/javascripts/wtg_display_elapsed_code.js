43715adfbfef02898212a0f5b48f924737e12cb3
Daniel Petkevich <daniel.petkevich@gmail.com>
2012-02-01 01:11:30 -0500

    fixed admin design

---
+++ public/javascripts/wtg_display_elapsed_code.js
@@ -0,0 +1,119 @@
+function updateElapsed( selector, interval ) {
+  // Sets an interval to update time elapsed for updates and the like
+  
+  if ( typeof interval === "undefined" ) {
+    interval = 16000; // Default to updating every 16 seconds
+  }
+  
+  var sI = setInterval( function() {
+  	  var toUpdate = $( selector );
+  
+  	  var now = new Date();
+  	  var obj;
+  	  var created;
+  	  var elapsed;
+  	  for ( var i = 0; i < toUpdate.length; i++ ) {
+        obj = toUpdate[ i ];
+        created = $( obj ).attr( 'data-datetime' );
+        created = new Date( created );
+        elapsed = now - created;
+        $( obj ).find( '.elapsed' ).html( displayElapsed( elapsed ) );
+  	  }
+	  }, interval );
+  
+}
+
+function displayElapsed( elapsed ) {
+  
+  var minute = 60000; // 60000 milliseconds in a minute
+  var hour = 60 * minute;
+  var day = 24 * hour;
+  var week = 7 * day;
+  var month = 30 * day;
+  var year = 365 * day;
+  
+  if ( elapsed < 10000 )
+    // If within the last 10 seconds, say "just now..."
+
+    return "just now...";
+  
+  else if ( elapsed < minute ) {
+    var seconds = Math.round( elapsed / 1000 );
+    
+    if ( seconds === 1 ) {
+      return "about 1 second ago";
+    } else {
+      return "about " + seconds + " seconds ago";
+    }
+
+  } else if ( elapsed < minute * 59.5 ) { // 59.5 minutes
+    
+    var minutes = Math.round( elapsed / minute );
+    
+    if ( minutes === 1 ) {
+      return "about 1 minute ago";
+    } else {
+      return "about " + minutes + " minutes ago";
+    }
+    
+    
+  } else if ( elapsed < hour * 23.5 ) {  // 23.5 hours
+    
+    var hours = Math.round( elapsed / hour );
+    
+    if ( hours === 1 ) {
+      return "about 1 hour ago";
+    } else {
+      return "about " + hours + " hours ago";
+    }
+    
+  } else if ( elapsed < day * 13.5 ) {  // 13.5 days
+    
+    var days = Math.round( elapsed / day );
+    
+    if ( days === 1 ) {
+      return "about 1 day ago";
+    } else {
+      return "about " + days + " days ago";
+    }
+  
+  } else if ( elapsed < week * 3.5 ) {  // 3.5 weeks
+    
+    var weeks = Math.round( elapsed / week );
+    
+    if ( weeks === 1 ) {
+      return "about 1 week ago";
+    } else {
+      return "about " + weeks + " weeks ago";
+    }
+  
+  } else if ( elapsed < month * 11.5 ) {  // 11.5 months
+
+    var months = Math.round( elapsed / month );
+    
+    if ( months === 1 ) {
+      return "about 1 month ago";
+    } else {
+      return "about " + months + " months ago";
+    }
+    
+  } else {
+
+    var years = Math.round( elapsed / year );
+
+    if ( years === 1 ) {
+      return "about 1 year ago";
+    } else {
+      return "about " + years + " years ago";
+    }
+    
+  }
+    
+}
+
+
+
+
+
+
+
