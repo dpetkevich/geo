895f8191169e55cfc71c721b8ae3637f77035d5a
Daniel Petkevich <daniel.petkevich@gmail.com>
2012-02-06 23:47:34 -0500

    secret admin url

--- routes.js
+++ routes.js
@@ -5,8 +5,8 @@ var routes = [
 	{ method: "get", path: "/", handler: posts_controller.index },
 	{ method: "post", path: "/", handler: posts_controller.create_post },
   	{ method: "get", path: "/get_posts", handler: posts_controller.get_posts }, // ADDED BY WTG
-  	{ method: "get", path: "/admin", handler: posts_controller.admin_list },
-  	{ method: "post", path: "/admin", handler: posts_controller.delete_post },
+  	{ method: "get", path: "/admin!!5423", handler: posts_controller.admin_list },
+  	{ method: "post", path: "/admin!!5423", handler: posts_controller.delete_post },
   	{ method: "get", path: "/get_admin_posts", handler: posts_controller.get_posts} 
  
 ]
